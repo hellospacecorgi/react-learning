eb1909485e3a4ce8693ebba33265bb6309d3f1f9
Flora Chan <chanhiuwun@hotmail.com>
2023-08-28 16:32:50 +1000

    Reorganising code, added sample jest fizzbuzz test

---
+++ src/fizzbuzz.js
@@ -0,0 +1,22 @@
+
+function fizz_buzz(numbers) {
+    let result = []
+    
+    for (var i = 0 ; i < numbers.length ; i++) {
+        var number = numbers[i];
+        if (number % 15 === 0) {
+            result.push('fizzbuzz')
+        } else if (number % 3 === 0) {
+            result.push('fizz')
+        } else if (number % 5 === 0) {
+            result.push('buzz')
+        } else {
+            result.push(number)
+        }
+    }
+    
+    return result.join(', ')
+  }
+  
+  module.exports = fizz_buzz;
+  
